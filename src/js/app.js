038fdd8fc87feb27c7d598655c84633990b7bb5d
paweltar <paweltar@gmail.com>
2017-05-25 17:46:45 +0200

    Include AOS library

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,18 @@
 import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
 import $ from 'jquery';
+import 'aos/dist/aos.css';
+import AOS from 'aos'
 import uikitCss from 'uikit/dist/css/uikit.css'
 
 UIkit.use(Icons);
 
-$(document).ready(function() {
+$(window).on("load", function(){
+  AOS.init({
+      disable: window.innerWidth < 960,
+      offset: 200,
+      duration: 600,
+      easing: 'ease-in-sine',
+  });
   console.log('hello');
 });
